7ace23c86c7204f6da876d4d011417b4b119865c
Rahul Sinha <rahul@idiv.in>
2025-09-06 12:41:22 +0530

    Added modash json

---
+++ examples/modash-response.js
@@ -0,0 +1,347 @@
+const modashResponse = {
+    "error": false,
+    "profile": {
+        "userId": "173560420",
+
+        // Available
+        "profile": {
+            "userId": "173560420",
+            "fullname": "Instagram",
+            "username": "instagram",
+            "url": "https://www.instagram.com/instagram/",
+            "picture": "https://imgigp.modash.io/?https://scontent-arn2-1.cdninstagram.com/t51.2885-19/s320x320/22159423_1794482210565499_9190891265074397184_n.jpg",
+            "followers": 313560626,
+            "engagements": 857994,
+            "engagementRate": 0.0027362938100525414
+        },
+
+        // After Signup: Not that important
+        "hashtags": [
+            {
+                "tag": "",
+                "weight": 0.1
+            }
+        ],
+
+        // After Signup: Not that important
+        "mentions": [
+            {
+                "tag": "",
+                "weight": 0.1
+            }
+        ],
+
+        // After Signup: We probably would fetch it after signup
+        "statsByContentType": {
+            "all": {},
+            "reels": {}
+        },
+
+        // After Signup: We can have Audience related data only after user signups our platform
+        "audience": {
+            "notable": 0.07,
+            "genders": [
+                {
+                    "code": "",
+                    "weight": 0.07
+                }
+            ],
+            "geoCountries": [
+                {
+                    "name": "",
+                    "weight": 0.07,
+                    "code": ""
+                }
+            ],
+            "ages": [
+                {
+                    "code": "",
+                    "weight": 0.07
+                }
+            ],
+            "gendersPerAge": [
+                {
+                    "code": "",
+                    "male": 0.07,
+                    "female": 0.07
+                }
+            ],
+            "geoCities": [
+                {
+                    "name": "",
+                    "weight": 0.07
+                }
+            ],
+            "geoStates": [
+                {
+                    "name": "",
+                    "weight": 0.07
+                }
+            ],
+            "credibility": 0.75,
+            "interests": [
+                {
+                    "name": "",
+                    "weight": 0.07
+                }
+            ],
+            "brandAffinity": [
+                {
+                    "name": "",
+                    "weight": 0.07
+                }
+            ],
+            "languages": [
+                {
+                    "code": "it",
+                    "name": "Italian",
+                    "weight": 0.1
+                }
+            ],
+            "notableUsers": [
+                {
+                    "userId": "173560420",
+                    "fullname": "Instagram",
+                    "username": "instagram",
+                    "url": "https://www.instagram.com/instagram/",
+                    "picture": "https://imgigp.modash.io/?https://scontent-arn2-1.cdninstagram.com/t51.2885-19/s320x320/22159423_1794482210565499_9190891265074397184_n.jpg",
+                    "followers": 313560626,
+                    "engagements": 857994
+                }
+            ],
+            "audienceLookalikes": [
+                {
+                    "userId": "173560420",
+                    "fullname": "Instagram",
+                    "username": "instagram",
+                    "url": "https://www.instagram.com/instagram/",
+                    "picture": "https://imgigp.modash.io/?https://scontent-arn2-1.cdninstagram.com/t51.2885-19/s320x320/22159423_1794482210565499_9190891265074397184_n.jpg",
+                    "followers": 313560626,
+                    "engagements": 857994
+                }
+            ],
+            "ethnicities": [
+                {
+                    "code": "white",
+                    "name": "White / Caucasian",
+                    "weight": 0.1
+                }
+            ],
+            "audienceReachability": [
+                {
+                    "code": "-500",
+                    "weight": 0.379536
+                },
+                {
+                    "code": "500-1000",
+                    "weight": 0.161691
+                },
+                {
+                    "code": "1000-1500",
+                    "weight": 0.097547
+                },
+                {
+                    "code": "1500-",
+                    "weight": 0.361225
+                }
+            ],
+            "audienceTypes": [
+                {
+                    "code": "mass_followers",
+                    "weight": 0.379536
+                },
+                {
+                    "code": "suspicious",
+                    "weight": 0.161691
+                },
+                {
+                    "code": "influencers",
+                    "weight": 0.097547
+                },
+                {
+                    "code": "real",
+                    "weight": 0.361225
+                }
+            ]
+        },
+
+        // Available
+        "stats": {
+            "avgLikes": {
+                "value": 859004,
+                "compared": 0.007144320631801482
+            },
+            "followers": {
+                "value": 859004,
+                "compared": 0.007144320631801482
+            },
+            "avgShares": {
+                "value": 859004,
+                "compared": 0.007144320631801482
+            },
+            "avgComments": {
+                "value": 859004,
+                "compared": 0.007144320631801482
+            }
+        },
+
+        // Available
+        "recentPosts": [
+            {
+                "id": "string",
+                "text": "string",
+                "url": "string",
+                "created": "string",
+                "likes": 0,
+                "comments": 0,
+                "mentions": [
+                    "string"
+                ],
+                "hashtags": [
+                    "string"
+                ]
+            }
+        ],
+
+        // Available
+        "popularPosts": [
+            {
+                "id": "string",
+                "text": "string",
+                "url": "string",
+                "created": "string",
+                "likes": 0,
+                "comments": 0,
+                "views": 0,
+                "thumbnail": "string",
+                "type": "string"
+            }
+        ],
+
+        // After Signup
+        "city": "New york",
+        // After Signup
+        "state": "California",
+
+        // Available
+        "gender": "FEMALE",
+
+        // After Signup
+        "language": {
+            "code": "en",
+            "name": "English"
+        },
+
+        // After Signup
+        "contacts": [
+            {
+                "type": "email",
+                "value": "influenceremail@example.com"
+            }
+        ],
+
+        // Available
+        "country": "US",
+
+        "ageGroup": "18-24",
+        "isPrivate": true,
+        "accountType": "Regular",
+        "isVerified": true,
+        "postsCounts": 37,
+        "avgLikes": 18211,
+        "avgComments": 12321,
+        "bio": "CEO of #RockTok",
+        "interests": [
+            {
+                "id": 1,
+                "name": ""
+            }
+        ],
+        "brandAffinity": [
+            {
+                "id": 1,
+                "name": ""
+            }
+        ],
+        "sponsoredPosts": [
+            {
+                "id": "string",
+                "text": "string",
+                "url": "string",
+                "created": "string",
+                "likes": 0,
+                "comments": 0,
+                "views": 0,
+                "mentions": [
+                    "string"
+                ],
+                "hashtags": [
+                    "string"
+                ],
+                "video": "string",
+                "image": "string",
+                "thumbnail": "string",
+                "sponsors": [
+                    {
+                        "domain": "string",
+                        "logo_url": "string",
+                        "name": "string"
+                    }
+                ]
+            }
+        ],
+        "statHistory": [
+            {
+                "month": "2019-05",
+                "followers": 1000,
+                "following": 1000,
+                "avgLikes": 1000,
+                "avgViews": 1000,
+                "avgComments": 1000,
+                "avgShares": 1000
+            }
+        ],
+        "lookalikes": [
+            {
+                "userId": "173560420",
+                "fullname": "Instagram",
+                "username": "instagram",
+                "url": "https://www.instagram.com/instagram/",
+                "picture": "https://imgigp.modash.io/?https://scontent-arn2-1.cdninstagram.com/t51.2885-19/s320x320/22159423_1794482210565499_9190891265074397184_n.jpg",
+                "followers": 313560626,
+                "engagements": 857994
+            }
+        ],
+        "postsCount": 37,
+        "avgViews": 48,
+        "avgReelsPlays": 5727,
+        "audienceExtra": {
+            "followersRange": {
+                "leftNumber": 100000,
+                "rightNumber": 500000
+            },
+            "engagementRateDistribution": [
+                {
+                    "min": 0.4546,
+                    "max": 0.6482,
+                    "total": 5551,
+                    "median": true
+                }
+            ],
+            "credibilityDistribution": [
+                {
+                    "min": 0.4546,
+                    "max": 0.6482,
+                    "total": 5551,
+                    "median": true
+                }
+            ]
+        },
+        "paidPostPerformance": 0.5,
+        "paidPostPerformanceViews": 37,
+        "sponsoredPostsMedianViews": 3127,
+        "sponsoredPostsMedianLikes": 3743,
+        "nonSponsoredPostsMedianViews": 267,
+        "nonSponsoredPostsMedianLikes": 367
+    }
+}
