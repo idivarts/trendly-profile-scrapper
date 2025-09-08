9fbe4b8559ef526e41577ea22cc9f089f3e7a385
Rahul Sinha <rahul@idiv.in>
2025-09-08 20:23:52 +0530

    Added some cleaning up

---
+++ constant.js
@@ -0,0 +1,36 @@
+const LeadAccountID = "rahul-sinha-mac"
+
+const GENDERS = [
+    "male",
+    "female",
+    "couple",
+    "baby",
+    "animal",
+    "lgbtq",
+    "gender-neutral",
+];
+
+const NICHES = [
+    "Fashion / Beauty",
+    "Lifestyle Vlogs",
+    "Food",
+    "Travel",
+    "Fun / Meme",
+    "Health",
+    "Tech",
+    "NSFW",
+    "Others",
+];
+
+const LOCATIONS = [
+    "Mumbai",
+    "Delhi",
+    "Bengaluru",
+    "Hyderabad",
+    "Ahmedabad",
+    "Chennai",
+    "Kolkata",
+    "Pune",
+    "Surat",
+    "Jaipur"
+];
