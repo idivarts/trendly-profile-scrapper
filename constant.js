8fbcd7e5bc227e110a574b5a6b07a5fab8a7556f
Rahul Sinha <rahul@idiv.in>
2026-02-14 00:16:54 +0530

    Updated the niches to be more inclusive

--- constant.js
+++ constant.js
@@ -2,37 +2,74 @@ const LeadAccountID = "data-intern-2"
 const IS_DEV = true
 
 const NICHES = [
-    "Fashion / Beauty",
-    "Lifestyle Vlogs",
-    "Food",
+    // Fashion & Appearance
+    "Fashion & Style",
+    "Beauty & Makeup",
+    "Skincare",
+
+    // Health & Body
+    "Fitness & Gym",
+    "Yoga & Wellness",
+    "Health & Nutrition",
+    "Mental Health",
+
+    // Food
+    "Food & Cooking",
+
+    // Lifestyle & Home
     "Travel",
-    "Fun / Comedy",
-    "Fitness",
-    "Gaming",
-    "Motivational",
-    "Finance",
-    "Education",
-    "Business",
-    "Photography",
-    "Cooking",
-    "Art",
+    "Lifestyle",
+    "Parenting & Family",
+    "Home & Interior",
+    "DIY & Crafts",
+    "Gardening & Plants",
+
+    // Creative & Arts
+    "Photography & Videography",
+    "Art & Illustration",
     "Music",
     "Dance",
-    "Film / TV",
+
+    // Entertainment
+    "Comedy & Skits",
+    "Memes & Humor",
+    "Gaming",
+    "Anime & Cosplay",
+    "Pop Culture & Entertainment",
+
+    // Knowledge & Education
+    "Tech & Gadgets",
+    "Science & Education",
+    "Books & Reading",
+    "Finance & Investing",
+    "Business & Entrepreneurship",
+    "Motivation & Self-Help",
+
+    // Sports & Outdoors
     "Sports",
-    "Automobile",
-    "Meme",
-    "Quote Pages",
-    "Parenting",
-    "Health",
-    "Tech",
-    "Pets",
-    "Dogs",
-    "Cats",
-    "NSFW",
-    "Short Story",
-    "Storytelling",
-    "Kids",
+    "Outdoor & Adventure",
+
+    // Niche Interest
+    "Pets & Animals",
+    "Automotive",
+    "Astrology & Spirituality",
+    "Real Estate",
+
+    // Identity & Community
+    "Body Positivity",
+    "LGBTQ+",
+    "Social Causes & Activism",
+    "Sustainability & Environment",
+
+    // Life Events & Luxury
+    "Wedding & Events",
+    "Luxury & High-End",
+
+    // Other Content Types
+    "Relationships & Dating",
+    "Quotes & Affirmations",
+    "Kids & Toys",
+    "NSFW & Adult",
     "Others",
 ];
 
